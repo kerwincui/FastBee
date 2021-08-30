e50ba21896d17f27bb4d9a5c5dfb8ca5c8a0fb5d
qianlile <qianlile@163.com>
2021-08-30 09:56:13 +0800

    删除无用文件夹

--- wechat/miniprogram/libs/wx-promisify.js
+++
@@ -1,8 +0,0 @@
-module.exports = api => (object, ...params) => new Promise((resolve, reject) => {
-  if (api) {
-    api.call(wx, { ...object, success: resolve, fail: reject }, ...params);
-  } else {
-    console.error('调用不支持的 API');
-    reject({ errCode: 'WXAPI_NOT_SUPPORTED' });
-  }
-});
