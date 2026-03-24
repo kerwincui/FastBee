abe46baf18ffaa1a20f7e1fe2098dc1049ec3a02
Zhunianya <2698076424@qq.com>
2026-03-24 11:29:16 +0800

    fix(国际化): 补充

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,10 +1,11 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
+import { i18n } from '@/lang';
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from '@/layout';
 
 /**
  * Note: 路由配置项
@@ -30,174 +31,174 @@ import Layout from '@/layout'
 
 // 公共路由
 export const constantRoutes = [
-  {
-    path: '/redirect',
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: '/redirect/:path(.*)',
-        component: () => import('@/views/redirect')
-      }
-    ]
-  },
     {
-    path: '/bigScreen',
-    component: () => import('@/views/bigScreen/home'),
-    hidden: true,
-    meta: { bigScreen: true }
-  },
-  {
-    path: '/login',
-    component: () => import('@/views/login'),
-    hidden: true
-  },
-  {
-    path: '/register',
-    component: () => import('@/views/register'),
-    hidden: true
-  },
-  {
-    path: '/404',
-    component: () => import('@/views/error/404'),
-    hidden: true
-  },
-  {
-    path: '/401',
-    component: () => import('@/views/error/401'),
-    hidden: true
-  },
-  {
-    path: '',
-    component: Layout,
-    redirect: 'index',
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/index'),
-        name: 'Index',
-        meta: { title: '首页', icon: 'dashboard', affix: true }
-      }
-    ]
-  },
-  {
-    path: '/user',
-    component: Layout,
-    hidden: true,
-    redirect: 'noredirect',
-    children: [
-      {
-        path: 'profile',
-        component: () => import('@/views/system/user/profile/index'),
-        name: 'Profile',
-        meta: { title: '个人中心', icon: 'user' }
-      }
-    ]
-  }
-]
+        path: '/redirect',
+        component: Layout,
+        hidden: true,
+        children: [
+            {
+                path: '/redirect/:path(.*)',
+                component: () => import('@/views/redirect'),
+            },
+        ],
+    },
+    {
+        path: '/bigScreen',
+        component: () => import('@/views/bigScreen/home'),
+        hidden: true,
+        meta: { bigScreen: true },
+    },
+    {
+        path: '/login',
+        component: () => import('@/views/login'),
+        hidden: true,
+    },
+    {
+        path: '/register',
+        component: () => import('@/views/register'),
+        hidden: true,
+    },
+    {
+        path: '/404',
+        component: () => import('@/views/error/404'),
+        hidden: true,
+    },
+    {
+        path: '/401',
+        component: () => import('@/views/error/401'),
+        hidden: true,
+    },
+    {
+        path: '',
+        component: Layout,
+        redirect: 'index',
+        children: [
+            {
+                name: 'Index',
+                path: 'index',
+                component: () => import('@/views/index'),
+                meta: { title: i18n.t('navbar.dashboard'), icon: 'home', affix: true },
+            },
+        ],
+    },
+    {
+        path: '/user',
+        component: Layout,
+        hidden: true,
+        redirect: 'noredirect',
+        children: [
+            {
+                path: 'profile',
+                component: () => import('@/views/system/user/profile/index'),
+                name: 'Profile',
+                meta: { title: i18n.t('navbar.personalCenter'), icon: 'user' },
+            },
+        ],
+    },
+];
 
 // 动态路由，基于用户权限动态去加载
 export const dynamicRoutes = [
-  {
-    path: '/system/user-auth',
-    component: Layout,
-    hidden: true,
-    permissions: ['system:user:edit'],
-    children: [
-      {
-        path: 'role/:userId(\\d+)',
-        component: () => import('@/views/system/user/authRole'),
-        name: 'AuthRole',
-        meta: { title: '分配角色', activeMenu: '/system/user' }
-      }
-    ]
-  },
-  {
-    path: '/system/role-auth',
-    component: Layout,
-    hidden: true,
-    permissions: ['system:role:edit'],
-    children: [
-      {
-        path: 'user/:roleId(\\d+)',
-        component: () => import('@/views/system/role/authUser'),
-        name: 'AuthUser',
-        meta: { title: '分配用户', activeMenu: '/system/role' }
-      }
-    ]
-  },
-  {
-    path: '/system/dict-data',
-    component: Layout,
-    hidden: true,
-    permissions: ['system:dict:list'],
-    children: [
-      {
-        path: 'index/:dictId(\\d+)',
-        component: () => import('@/views/system/dict/data'),
-        name: 'Data',
-        meta: { title: '字典数据', activeMenu: '/system/dict' }
-      }
-    ]
-  },
-  {
-    path: '/monitor/job-log',
-    component: Layout,
-    hidden: true,
-    permissions: ['monitor:job:list'],
-    children: [
-      {
-        path: 'index/:jobId(\\d+)',
-        component: () => import('@/views/monitor/job/log'),
-        name: 'JobLog',
-        meta: { title: '调度日志', activeMenu: '/monitor/job' }
-      }
-    ]
-  },
-  {
-    path: '/tool/gen-edit',
-    component: Layout,
-    hidden: true,
-    permissions: ['tool:gen:edit'],
-    children: [
-      {
-        path: 'index/:tableId(\\d+)',
-        component: () => import('@/views/tool/gen/editTable'),
-        name: 'GenEdit',
-        meta: { title: '修改生成配置', activeMenu: '/tool/gen' }
-      }
-    ]
-  },
-  {
-    path: '/iot',
-    component: Layout,
-    hidden: true,
-    permissions: ['iot:device:add'],
-    children: [
-      {
-        path: 'product-edit',
-        component: () => import('@/views/iot/product/product-edit'),
-        name: 'ProductEdit',
-        meta: { title: '编辑产品', activeMenu: '/iot/product',nocache: true}
-      },
-      {
-        path: 'device-edit',
-        component: () => import('@/views/iot/device/device-edit'),
-        name: 'DeviceEdit',
-        meta: { title: '编辑设备', activeMenu: '/iot/device', noCache: true}
-      }
-    ]
-  },
-]
+    {
+        path: '/system/user-auth',
+        component: Layout,
+        hidden: true,
+        permissions: ['system:user:edit'],
+        children: [
+            {
+                path: 'role/:userId(\\d+)',
+                component: () => import('@/views/system/user/authRole'),
+                name: 'AuthRole',
+                meta: { title: i18n.t('navbar.assignRoles'), activeMenu: '/system/user' },
+            },
+        ],
+    },
+    {
+        path: '/system/role-auth',
+        component: Layout,
+        hidden: true,
+        permissions: ['system:role:edit'],
+        children: [
+            {
+                path: 'user/:roleId(\\d+)',
+                component: () => import('@/views/system/role/authUser'),
+                name: 'AuthUser',
+                meta: { title: i18n.t('navbar.assignUsers'), activeMenu: '/enterprise/role' },
+            },
+        ],
+    },
+    {
+        path: '/system/dict-data',
+        component: Layout,
+        hidden: true,
+        permissions: ['system:dict:list'],
+        children: [
+            {
+                path: 'index/:dictId(\\d+)',
+                component: () => import('@/views/system/dict/data'),
+                name: 'Data',
+                meta: { title: i18n.t('navbar.dictionaryData'), activeMenu: '/system/dict' },
+            },
+        ],
+    },
+    {
+        path: '/monitor/job-log',
+        component: Layout,
+        hidden: true,
+        permissions: ['monitor:job:list'],
+        children: [
+            {
+                path: 'index/:jobId(\\d+)',
+                component: () => import('@/views/monitor/job/log'),
+                name: 'JobLog',
+                meta: { title: i18n.t('navbar.dispatchingLog'), activeMenu: '/monitor/job' },
+            },
+        ],
+    },
+    {
+        path: '/tool/gen-edit',
+        component: Layout,
+        hidden: true,
+        permissions: ['tool:gen:edit'],
+        children: [
+            {
+                path: 'index/:tableId(\\d+)',
+                component: () => import('@/views/tool/gen/editTable'),
+                name: 'GenEdit',
+                meta: { title: i18n.t('navbar.modifyGenerationConfiguration'), activeMenu: '/tool/gen' },
+            },
+        ],
+    },
+    {
+        path: '/iot',
+        component: Layout,
+        hidden: true,
+        permissions: ['iot:device:add'],
+        children: [
+            {
+                path: 'product-edit',
+                component: () => import('@/views/iot/product/product-edit'),
+                name: 'ProductEdit',
+                meta: { title: i18n.t('navbar.editingProduct'), activeMenu: '/iot/product/list', noCache: true },
+            },
+            {
+                path: 'device-edit',
+                component: () => import('@/views/iot/device/device-edit'),
+                name: 'DeviceEdit',
+                meta: { title: i18n.t('navbar.editingEquipment'), activeMenu: '/iot/device/list', noCache: true },
+            },
+        ],
+    },
+];
 
 // 防止连续点击多次路由报错
 let routerPush = Router.prototype.push;
 Router.prototype.push = function push(location) {
-  return routerPush.call(this, location).catch(err => err)
-}
+    return routerPush.call(this, location).catch((err) => err);
+};
 
 export default new Router({
-  mode: 'history', // 去掉url中的#
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
+    mode: 'history', // 去掉url中的#
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes,
+});
